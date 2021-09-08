d0196610e2c670fd03577d5fac7037ba03086206
revenths <reventhkumar76@gmail.com>
2021-09-08 09:31:30 +0530

    password Functionality

--- ProjectGit/src/org/source/BaseClass.java
+++ ProjectGit/src/org/source/BaseClass.java
@@ -1,5 +1,12 @@
 package org.source;
 
 public class BaseClass {
+	public static void password() {
+		System.out.println("password");
+		
+	}
+	public static void main(String[] args) {
+		password();
+	}
 
 }
