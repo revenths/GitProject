bbf19712c9b53a630c5a7f1e811ab324b6519cbe
revenths <reventhkumar76@gmail.com>
2021-09-08 09:35:26 +0530

    password Functionality

--- ProjectGit/src/org/origin/Loigin.java
+++ ProjectGit/src/org/origin/Loigin.java
@@ -1,5 +1,14 @@
 package org.origin;
 
 public class Loigin {
+	
+	public static void username() {
+		System.out.println("username");
+		
+	}
+	public static void main(String[] args) {
+		username();
+	}
 
 }
+
