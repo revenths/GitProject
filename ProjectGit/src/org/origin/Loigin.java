73111cf4dc55e07e33eea184a9266316fc0c5e54
revenths <reventhkumar76@gmail.com>
2021-09-09 06:36:59 +0530

    conflict resolved

--- ProjectGit/src/org/origin/Loigin.java
+++ ProjectGit/src/org/origin/Loigin.java
@@ -2,20 +2,20 @@ package org.origin;
 
 public class Loigin {
 	
-	public static void username1() {
+	public static void username() {
 		System.out.println("username");
 		
 	}
 	public static void main1(String[] args) {
-		username1();
+		username();
 	}
 
-	public static void username() {
-		System.out.println("username");
+	public static void password() {
+		System.out.println("password");
+		
 	}
-	
 	public static void main(String[] args) {
-		username1();
+		password();
 	}
 	
 }
